54972ef92caf1a3df2d2a1ec5b7434872c5bfcea
saitoal <70146636+saitoal@users.noreply.github.com>
2020-08-27 17:40:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://java-api-spring-bot.herokuapp.com/validacpf?cpf="
+const URL="https://itauturma7a-saitoal.herokuapp.com/validacpf?cpf="
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
